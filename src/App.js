725e6e1f76aaf53fa26817c5ffade28b51d2457a
Piotr Szulc <p.szulc@millionmonkeyssoftware.com>
2019-10-13 17:45:47 +0200

    colors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { isNil } from 'ramda';
 import './App.css';
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -61,8 +70,18 @@ class App extends React.Component {
     const left = (vote.x + 10) / 20 * 400;
     const top = 400 - ((vote.y + 10) / 20 * 400);
 
+    const { hoveredIndex } = this.state;
+
+    let selected = false;
+
+    if (hoveredIndex === index) {
+        selected = true;
+    }
+
+    const color = (vote.class === 'Computer Science'?  'black': 'orange');
+
     return (
-        <div className="vote" style={{left, top, backgroundColor: vote.class === 'Computer Science'?  'black': 'orange'}}
+        <div className="vote" style={{ opacity: 0.7, left, top, backgroundColor: selected ? 'yellow' : 'black'}}
              onMouseEnter={() => this.setState(state => ({hoveredIndex: index}))}
              onMouseLeave={() => this.setState(state => ({hoveredIndex: undefined}))}
                  />
